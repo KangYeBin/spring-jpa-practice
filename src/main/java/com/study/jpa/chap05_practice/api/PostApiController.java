57eb10e99a43b20fef94352b98c2fce55d5015d0
yb <yb725v@naver.com>
2024-05-14 17:48:03 +0900

    종합 연습 예제 (posts -> GET) 처리

--- src/main/java/com/study/jpa/chap05_practice/api/PostApiController.java
+++ src/main/java/com/study/jpa/chap05_practice/api/PostApiController.java
@@ -1,8 +1,11 @@
 package com.study.jpa.chap05_practice.api;
 
+import com.study.jpa.chap05_practice.dto.PageDTO;
+import com.study.jpa.chap05_practice.dto.PostListResponseDTO;
 import com.study.jpa.chap05_practice.service.PostService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -21,5 +24,14 @@ public class PostApiController {
         게시물 수정:     /posts/{id}       - PATCH
         게시물 삭제:     /posts/{id}       - DELETE
     */
+
+    @GetMapping
+    public ResponseEntity<?> list(PageDTO pageDTO) {
+        log.info("/api/v1/posts?page={}&size={}", pageDTO.getPage(), pageDTO.getSize());
+
+        PostListResponseDTO dto = postService.getPosts(pageDTO);
+
+        return ResponseEntity.ok().body(dto);
+    }
 
 }
