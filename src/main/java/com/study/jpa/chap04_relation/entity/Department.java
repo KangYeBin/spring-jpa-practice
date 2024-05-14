9ab5a1261370dbce2517360a1e4b3fd4d5ce5cdc
yb <yb725v@naver.com>
2024-05-14 15:06:56 +0900

    JPA 연관관계 매핑(양방향), FETCH JOIN(N+1 문제)

--- src/main/java/com/study/jpa/chap04_relation/entity/Department.java
+++ src/main/java/com/study/jpa/chap04_relation/entity/Department.java
@@ -3,9 +3,12 @@ package com.study.jpa.chap04_relation.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter
 @Setter
-@ToString
+@ToString(exclude = {"employees"})
 @EqualsAndHashCode(of = "id")
 @NoArgsConstructor
 @AllArgsConstructor
@@ -22,4 +25,9 @@ public class Department {
     @Column(name = "dept_name", nullable = false)
     private String name;
 
+    // 양방향 매핑에서는 상대방 엔터티의 갱신에 관여할 수 없다.
+    // 읽기 전용(조회)으로만 사용해야 한다.
+    // mappedBy에는 상대방 엔터티의 조인되는 필드명 작성
+    @OneToMany(mappedBy = "department")
+    private List<Employee> employees = new ArrayList<>();    // NPE 방지를 위해 초기화 필요
 }
--- src/main/java/com/study/jpa/chap04_relation/entity/Employee.java
+++ src/main/java/com/study/jpa/chap04_relation/entity/Employee.java
@@ -5,7 +5,8 @@ import lombok.*;
 
 @Getter
 @Setter
-@ToString
+// JPA 연관관계 매핑에서 연관관계 데이터는 toString에서 제외해야 한다 (순환 참조 발생, 무한 루프 방지)
+@ToString(exclude = {"department"})
 @EqualsAndHashCode(of = "id")
 @NoArgsConstructor
 @AllArgsConstructor
@@ -23,7 +24,10 @@ public class Employee {
     private String name;
 
     // Employee와 Department는 N:1 관계이므로 ManyToOne
-    @ManyToOne
+    // 외래 키를 가진 Employee는 Department 테이블의 갱신에 관여할 수 있다
+    // LAZY : 필요한 경우에만 조인 수행
+    // EAGER : 항상 무조건 조인 수행
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "dept_id")
     private Department department;
 }
